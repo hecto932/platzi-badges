a3f1dbc224c2b8832d21f133bbded0b315964475
Hector Flores <hecto932@gmail.com>
2019-03-31 21:26:15 -0600

    How to apply styles

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,28 +1,31 @@
 import React from 'react';
 
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg'
 
 class Badge extends React.Component {
   render() {
-    return <div>
-      <div>
-        <img src={confLogo} alt="Logo de la conferencia"/>
-      </div>
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
 
-      <div>
-        <img src="https://www.gravatar.com/avatar?d=identicon" alt="avatar"/>
-        <h1>Hector <br/> Flores</h1>
-      </div>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="avatar" />
+          <h1>Hector <br /> Flores</h1>
+        </div>
 
-      <div>
-        <p>Software Engineer</p>
-        <p>@hecto932</p>
-      </div>
+        <div className="Badge__section-info">
+          <h3>Software Engineer</h3>
+          <div>@hecto932</div>
+        </div>
 
-      <div>
-        #platziconf
+        <div className="Badge__footer">
+          #platziconf
+        </div>
       </div>
-    </div>
+    )
   }
 }
 
