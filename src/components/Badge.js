c7321d53f32ef241d70dbe188752e30f1dead1f1
Hector Flores <hecto932@gmail.com>
2019-04-14 12:59:13 -0500

    Sending post data

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import './styles/Badge.css';
-import confLogo from '../images/badge-header.svg'
+import "./styles/Badge.css";
+import confLogo from "../images/badge-header.svg";
+import Gravatar from '../components/Gravatar';
 
 class Badge extends React.Component {
   render() {
@@ -12,8 +13,14 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-name">
-          <img className="Badge__avatar" src={this.props.avatarUrl} alt="avatar" />
-          <h1>{this.props.firstName} <br /> {this.props.lastName}</h1>
+          <Gravatar 
+            className="Badge__avatar"
+            email={this.props.email}
+            alt="Avatar"
+          />
+          <h1>
+            {this.props.firstName} <br /> {this.props.lastName}
+          </h1>
         </div>
 
         <div className="Badge__section-info">
@@ -21,11 +28,9 @@ class Badge extends React.Component {
           <div>@{this.props.twitter}</div>
         </div>
 
-        <div className="Badge__footer">
-          #platziconf
-        </div>
+        <div className="Badge__footer">#platziconf</div>
       </div>
-    )
+    );
   }
 }
 
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,9 +1,12 @@
 import React, { Component, Fragment } from "react";
+import md5 from 'md5';
 
 import './styles/BadgeNew.css';
-import header from '../images/badge-header.svg';
+import header from '../images/platziconf-logo.svg';
 import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
+
+import api from '../api';
 
 class BadgeNew extends Component {
   state = {
@@ -12,9 +15,21 @@ class BadgeNew extends Component {
       lastName: '',
       email: '',
       jobTitle: '',
-      twitter: ''
+      twitter: '',
+      avatarUrl: '',
     } 
   };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ loading: true, error: null });
+    try {
+      await api.badges.create(this.state.form);
+      this.setState({ loading: false });
+    } catch (err) {
+      this.setState({ loading: false, error: err })
+    }
+  }
 
   handleChange = e => {
     // const nextForm = this.state.form;
@@ -22,36 +37,41 @@ class BadgeNew extends Component {
     // this.setState({
     //   form: nextForm
     // })
+    const email = this.state.form.email
+    const hash = md5(email);
     this.setState({
       form: {
         ...this.state.form,
+        avatarUrl: `https://s.gravatar.com/avatar/${hash}?d=identicon`,
         [e.target.name]: e.target.value
       }
     });
   };
   render() {
+    
     return (
       <Fragment>
         <div className="BadgeNew__hero">
-          <img className="img-fluid" src={header} alt="Logo " />
+          <img className="BadgeNew__hero-image img-fluid" src={header} alt="Logo " />
         </div>
 
         <div className="container">
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName={this.state.form.firstName}
-                lastName={this.state.form.lastName}
-                email={this.state.form.email}
-                twitter={this.state.form.twitter}
-                jobTitle={this.state.form.jobTitle}
-                avatarUrl="https://s.gravatar.com/avatar/c7fbfbcf4ded30f0cd55029f7c43205d?s=80"
+                firstName={this.state.form.firstName || 'FIRSTNAME'}
+                lastName={this.state.form.lastName || 'LASTNAME'}
+                email={this.state.form.email || 'EMAIL'}
+                twitter={this.state.form.twitter || 'Twitter'}
+                jobTitle={this.state.form.jobTitle || 'JOB_TITLE'}
+                avatarUrl={this.state.form.avatarUrl}
               />
             </div>
             <div className="col-6">
               <BadgeForm
                 onChange={this.handleChange}
                 formValues={this.state.form}
+                onSubmit={this.handleSubmit}
               />
             </div>
           </div>
