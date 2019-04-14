7a07984d2b9c8ae8c6bb0e8e47abe9b776b4a45a
Hector Flores <hecto932@gmail.com>
2019-04-14 01:22:53 -0500

    Enhance the UX

--- src/api.js
+++ src/api.js
@@ -25,8 +25,8 @@ const api = {
   badges: {
     list() {
       // return [];
-      // throw new Error('Not found');
-      return callApi('/badges');
+      throw new Error('Not found');
+      // return callApi('/badges');
     },
     create(badge) {
       return callApi(`/badges`, {
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -5,6 +5,8 @@ import './styles/Badges.css';
 import confLogo from '../images/badge-header.svg';
 import Navbar from '../components/Navbar';
 import BadgesList from '../components/BadgesList';
+import PageLoading from '../components/PageLoading';
+import PageError from '../components/PageError'
 
 import api from '../api';
 
@@ -61,11 +63,11 @@ class Badges extends Component {
   render() {
     console.log('2/4. render');
     if (this.state.loading === true) {
-      return 'Loading...';
+      return <PageLoading />
     }
 
     if (this.state.error) {
-      return `Error: ${this.state.error.message}`;
+      return <PageError error={this.state.error} />
     }
 
     return (
