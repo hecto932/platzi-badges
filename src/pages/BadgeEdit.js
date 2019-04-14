2ea472f69fa7d0587c9e884a2425c4e90db7bbdc
Hector Flores <hecto932@gmail.com>
2019-04-14 18:33:31 -0500

    Updating data PUT

---
+++ src/pages/BadgeEdit.js
@@ -0,0 +1,109 @@
+import React, { Component, Fragment } from "react";
+import md5 from "md5";
+
+import "./styles/BadgeEdit.css";
+import header from "../images/platziconf-logo.svg";
+import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
+import PageLoading from "../components/PageLoading";
+
+import api from "../api";
+
+class BadgeEdit extends Component {
+  state = {
+    loading: true,
+    error: null,
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: ""
+    }
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async e => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const data = await api.badges.read(this.props.match.params.badgeId);
+
+      this.setState({ loading: false, form: data });
+    } catch (err) {
+      this.setState({ loading: false, error: err });
+    }
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ loading: true, error: null });
+    try {
+      await api.badges.update(this.props.match.params.badgeId, this.state.form);
+      this.setState({ loading: false });
+      this.props.history.push("/badges");
+    } catch (err) {
+      this.setState({ loading: false, error: err });
+    }
+  };
+
+  handleChange = e => {
+    // const nextForm = this.state.form;
+    // nextForm[e.target.name] = e.target.value;
+    // this.setState({
+    //   form: nextForm
+    // })
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+  render() {
+    if (this.state.loading) {
+      return <PageLoading />;
+    }
+
+    return (
+      <Fragment>
+        <div className="BadgeEdit__hero">
+          <img
+            className="BadgeEdit__hero-image img-fluid"
+            src={header}
+            alt="Logo "
+          />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName={this.state.form.firstName || "FIRSTNAME"}
+                lastName={this.state.form.lastName || "LASTNAME"}
+                email={this.state.form.email || "EMAIL"}
+                twitter={this.state.form.twitter || "Twitter"}
+                jobTitle={this.state.form.jobTitle || "JOB_TITLE"}
+                avatarUrl={this.state.form.avatarUrl}
+              />
+            </div>
+            <div className="col-6">
+              <h1>Edit Attendant</h1>
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+                onSubmit={this.handleSubmit}
+                error={this.state.error}
+              />
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default BadgeEdit;
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -71,6 +71,7 @@ class BadgeNew extends Component {
               />
             </div>
             <div className="col-6">
+              <h1>New Attendant</h1>
               <BadgeForm
                 onChange={this.handleChange}
                 formValues={this.state.form}
